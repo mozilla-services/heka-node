b3aad5666054747b4d08c5c2f9a388d57f27d7d6
Rob Miller <rob@kalistra.com>
2012-05-21 13:43:46 -0700

    initial config module and initial tests

---
+++ config.js
@@ -0,0 +1,98 @@
+/*
+ ***** BEGIN LICENSE BLOCK *****
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this file,
+ * You can obtain one at http://mozilla.org/MPL/2.0/.
+ *
+ * The Initial Developer of the Original Code is the Mozilla Foundation.
+ * Portions created by the Initial Developer are Copyright (C) 2012
+ * the Initial Developer. All Rights Reserved.
+ *
+ * Contributor(s):
+ *  Rob Miller (rmiller@mozilla.com)
+ *
+ ***** END LICENSE BLOCK *****
+ */
+"use strict";
+var clientModule = require('./client');
+var globalConfig = {};
+
+var getattr = function(obj, attr, defaultValue) {
+    defaultValue = typeof(defaultValue) !== 'undefined' ? defaultValue : {};
+    var value = typeof(obj[attr]) !== 'undefined' ? obj[attr] : defaultValue;
+    return value;
+};
+
+/*
+* Fetch a module attribute by name and return the actual object (usually
+* a function) to which the name refers.
+*
+* @param name: String referring to the callable. Should consist of a module
+*              name (can be a bare name or a file path, anything that will
+*              work with `require`) and an exported module attribute name
+*              separated by a colon. For example, this function itself would
+*              be specified by './config:resolveName'.
+*/
+var resolveName = function(name) {
+    var pieces = name.split(':');
+    var module = require(pieces[0]);
+    var fn = module[pieces[1]];
+    return fn;
+};
+
+
+var senderFromConfig = function(config) {
+    // TODO: enforce check for `factory` value
+    var senderFactory = resolveName(config.factory);
+    return senderFactory(config);
+};
+
+var clientFromJsonConfig = function(config, client, clearGlobal) {
+    config = JSON.parse(config);
+    var senderConfig = getattr(config, 'sender');
+    var sender = senderFromConfig(senderConfig);
+    var logger = getattr(config, 'logger', '');
+    var severity = getattr(config, 'severity', 6);
+    var disabledTimers = getattr(config, 'disabledTimers', []);
+    var plugins = getattr(config, 'plugins');
+    var newGlobals = getattr(config, 'global');
+
+    clearGlobal = typeof(clearGlobal) !== 'undefined' ? clearGlobal : false;
+    if (clearGlobal) {
+        globalConfig = {};
+    };
+    for (var attr in newGlobals) {
+        if (newGlobals.hasOwnProperty(attr)) {
+            globalConfig[attr] = newGlobals[attr];
+        };
+    };
+
+    var filterNames = getattr(config, 'filters', []);
+    var filters = [];
+    for (var i=0; i<filterNames.length; i++) {
+        var fn = resolveName(filterNames[i][0]);
+        filters.push([fn, resolveName(filternames[i][1])]);
+    };
+
+    if (typeof(client) === 'undefined') {
+        client = new clientModule.MetlogClient(sender, logger, severity,
+                                               disabledTimers, filters);
+    } else {
+        client.setup(sender, logger, severity, disabledTimers, filters);
+    };
+
+    for (var name in plugins) {
+        if (plugins.hasOwnProperty(name)) {
+            var pluginConfig = plugin[name];
+            var provider = pluginConfig.provider;
+            var pluginMethod = provider(pluginConfig);
+            client.addMethod(name, pluginFn);
+        };
+    };
+
+    return client;
+};
+
+exports.globalConfig = globalConfig;
+exports.resolveName = resolveName;
+exports.clientFromJsonConfig = clientFromJsonConfig;
---
+++ tests/config.spec.js
@@ -0,0 +1,120 @@
+/*
+ ***** BEGIN LICENSE BLOCK *****
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this file,
+ * You can obtain one at http://mozilla.org/MPL/2.0/.
+ *
+ * The Initial Developer of the Original Code is the Mozilla Foundation.
+ * Portions created by the Initial Developer are Copyright (C) 2012
+ * the Initial Developer. All Rights Reserved.
+ *
+ * Contributor(s):
+ *  Rob Miller (rmiller@mozilla.com)
+ *
+ ***** END LICENSE BLOCK *****
+ */
+"use strict";
+
+var configModule = require('../config');
+
+var mockSender = {
+    foo: 'bar',
+    msgs: [],
+    sendMessage: function(msg) {
+        this.msgs.push(msg);
+    },
+    reset: function() {
+        this.foo = 'bar';
+        this.msgs = [];
+    }
+};
+
+var makeMockSender = function(senderConfig) {
+    if (typeof(senderConfig.foo) !== 'undefined') {
+        mockSender.foo = senderConfig.foo;
+    };
+    return mockSender;
+};
+var makeMockSenderString = './tests/config.spec:makeMockSender';
+
+describe('config', function() {
+
+    beforeEach(function() {
+        mockSender.reset();
+    });
+
+    it('sets up a basic client', function() {
+        var config = {
+            'sender': {'factory': makeMockSenderString},
+            'logger': 'test',
+            'severity': 5
+        };
+        var jsonConfig = JSON.stringify(config);
+        var client = configModule.clientFromJsonConfig(jsonConfig);
+        expect(client.logger).toEqual(config.logger);
+        expect(client.sender).toBe(mockSender);
+        expect(client.sender.foo).toEqual('bar');
+        expect(client.severity).toEqual(config.severity);
+        var msgOpts = {'payload': 'whadidyusay?'};
+        var type = 'test-type'
+        client.metlog(type, msgOpts);
+        expect(client.sender.msgs.length).toEqual(1);
+        var msg = client.sender.msgs[0]
+        expect(msg.type).toEqual(type);
+        expect(msg.payload).toEqual(msgOpts.payload);
+        expect(msg.severity).toEqual(config.severity);
+    });
+
+    it('configures sender correctly', function() {
+        var config = {
+            'sender': {'factory': makeMockSenderString,
+                       'foo': 'bawlp'},
+            'logger': 'test'
+        };
+        var jsonConfig = JSON.stringify(config);
+        var client = configModule.clientFromJsonConfig(jsonConfig);
+        expect(mockSender.foo).toEqual('bawlp');
+    });
+
+    it('sets, updates, and clears globals', function() {
+        var mergeObjects = function(obs) {
+            var result = {};
+            for (var i=0; i<obs.length; i++) {
+                var ob = obs[i];
+                for (var attr in ob) {
+                    if (ob.hasOwnProperty(attr)) {
+                        result[attr] = ob[attr];
+                    };
+                };
+            };
+            return result;
+        };
+
+        var globalParam1 = {'some': 'globalvalue'};
+        var config1 = {
+            'sender': {'factory': makeMockSenderString},
+            'logger': 'test',
+            'global': globalParam1
+        };
+        var jsonConfig = JSON.stringify(config1);
+        var client = configModule.clientFromJsonConfig(jsonConfig);
+        expect(configModule.globalConfig).toEqual(globalParam1);
+
+        var globalParam2 = {'another': 'globalsetting'};
+        var config2 = {
+            'sender': {'factory': makeMockSenderString},
+            'logger': 'test',
+            'global': globalParam2
+        };
+        jsonConfig = JSON.stringify(config2)
+        client = configModule.clientFromJsonConfig(jsonConfig, client);
+        var expected = mergeObjects([globalParam1, globalParam2]);
+        expect(configModule.globalConfig).toEqual(expected);
+
+        debugger;
+        client = configModule.clientFromJsonConfig(jsonConfig, client, true);
+        expect(configModule.globalConfig).toEqual(globalParam2);
+    });
+});
+
+exports.makeMockSender = makeMockSender;
